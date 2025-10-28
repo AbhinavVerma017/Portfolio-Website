bada611eb14f12cb5e72e4d1469724df4e878ae8
abhinav.verma <abhinav.verma@esecforte.com>
2025-10-28 23:24:25 +0530

    Update the cv

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,7 +2,7 @@ export const experienceData = [
     {
         id: 1,
         company: 'eSec Forte Technologies',
-        jobtitle: 'Software Engineer',
+        jobtitle: 'Software Engineer 1',
         startYear: '2024',
         endYear: 'Present'
     },
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -9,12 +9,22 @@ import eight from '../assets/svg/projects/eight.svg'
 import fifteen from '../assets/svg/projects/fifteen.svg'
 import sixteen from '../assets/svg/projects/sisxteen.svg'
 import seventeen from '../assets/svg/projects/seventeen.svg'
-
+import twenty from '../assets/svg/projects/twenty.svg'
+import nineteen from '../assets/svg/projects/nineteen.svg'
 
 
 export const projectsData = [
     {
         id: 1,
+        projectName: 'URL Shortener',
+        projectDesc: 'Built a Bitly-like URL shortener with cache-first redirects and real-time analytics.',
+        tags: ['Node.js', 'Express', 'MongoDB', 'Redis'],
+        code: 'https://github.com/AbhinavVerma017/Url-Shortner',
+        demo: 'https://url-shortner-r87k.onrender.com/',
+        image: twenty
+    },
+    {
+        id: 2,
         projectName: 'Voice-Based Emailing System',
         projectDesc: 'Built voice-enabled email management using speech recognition APIs to enable hands-free interaction.',
         tags: ['React', 'Node JS' , 'Mongo DB', 'Postgre SQL'],
@@ -23,16 +33,16 @@ export const projectsData = [
         image: fifteen
     },
     {
-        id: 2,
+        id: 3,
         projectName: 'Netflix Clone',
         projectDesc: 'A front-end clone of Netflix built from scratch using React JS. App uses Firebase for user authentication and Firestore Database .',
-        tags: ['React', 'CSS' , 'Firebase'],
+        tags: ['React', 'Redux', 'CSS' , 'Firebase'],
         code: 'https://github.com/AbhinavVerma017/netflix_clone',
-        demo: '',
+        demo: 'https://netflix-clone-zeta-sand.vercel.app/',
         image: sixteen
     },
     {
-        id: 3,
+        id: 4,
         projectName: 'Risk Life Cycle Management (RLCM)',
         projectDesc: ' Risk Life Cycle Management (RLCM) is a system used in audits to identify, assess, monitor, and mitigate risks throughout the entire audit process, ensuring effective risk control and compliance.',
         tags: ['Angular', 'Node JS', 'PHP' , 'My SQL'],
@@ -41,12 +51,21 @@ export const projectsData = [
         image: three
     },
     {
-        id: 4,
+        id: 5,
         projectName: 'News Vlog Application',
         projectDesc: 'This project is a news blog application built using PHP CodeIgniter 3 and RESTful APIs to perform CRUD (Create, Read, Update, Delete) operations on news content.',
         tags: ['PHP'],
         code: 'https://github.com/AbhinavVerma017/News_Blog-Application',
         demo: '',
+        image: nineteen
+    },
+    {
+        id: 6,
+        projectName: 'Portfloio Website',
+        projectDesc: 'This is a personal portfolio website built using React. It showcases my projects, skills, and experience in a clean and modern design. It serves as my professional online presence.',
+        tags: ['React js , Email JS'],
+        code: 'https://github.com/AbhinavVerma017/Portfolio-Website',
+        demo: 'https://portfolio-website-six-coral.vercel.app/',
         image: seventeen
     }
 ]
